81943374f7f15cc2a3484b123ef0018d704a664d
Pablo Muñoz <pablerass@gmail.com>
2018-08-19 14:31:51 +0200

    Make it work with molecule

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,11 +8,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_socket(host):
+    """Test socket connection."""
     s = host.socket("tcp://0.0.0.0:9090")
     assert s.is_listening
 
 
 def test_version(host):
-    version = os.getenv("PROMETHEUS", "2.3.2")
+    """Test version."""
+    version = os.getenv('PROMETHEUS', "2.3.2")
     out = host.run("/opt/prometheus/prometheus --version").stderr
     assert "prometheus, version " + version in out
